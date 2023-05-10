58f4d07cdcbe2e40827cdb06241e45ea34dd16f7
reidhin <hansweda@gmail.com>
2023-05-10 20:59:17 +0200

    Toevoegen klas/groep aan beschrijving

--- data-raw/contents.R
+++ data-raw/contents.R
@@ -1,10 +1,13 @@
 # create contents dataset
 
 contents <- data.frame(
-  school = c("NSV2", "Kandinsky", "NSV2"),
+  school = c("NSV2 - groep 7", "Kandinsky - klas 2", "NSV2 - groep 7"),
   region = c("Europa", "Europa", "Noord-Europa"),
   filename = c("small_europe.rds", "europe.rds", "northern_europe.rds")
 )
+
+# load the package
+devtools::load_all()
 
 
 # save as rds
